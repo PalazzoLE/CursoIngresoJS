58ec0646616ed75461e54b494e60d76759ffdef8
Lucas Palazzo <lucase.palazzo@gmail.com>
2018-02-05 20:54:42 -0300

    ejercicio 1 y 2
    Ver el alert, prompt, variables, y concatenar

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,18 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	//alert("anda");
+	//toda linea de instruccion, termina con punto y coma (;)
+	// var = porcion de memoria ram
 	
+	var nombre = prompt("ingrese su nombre","natalia natalia");
+	alert("Hola "+nombre+ ", mucho gusto");
+
+	/*
+	var nombre;
+	alert("nombre");
+	alert(nombre);
+	nombre = "Lucas";
+	alert(nombre);*/
 }
 
